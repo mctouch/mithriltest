5ba124acf22c25a2373cd34d6789fa26b9530035
mctouch <mctouched@gmail.com>
2019-04-27 14:29:55 +1000

    final

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import m from "mithril";
-m.render(document.body, "hello world");
+// src/index.js
+var m = require("mithril")
+
+var UserList = require("./views/UserList")
+var UserForm = require("./views/UserForm")
+var Layout = require("./views/Layout")
+
+m.route(document.body, "/list", {
+    "/list": {
+        render: function() {
+            return m(Layout, m(UserList))
+        }
+    },
+    "/edit/:id": {
+        render: function(vnode) {
+            return m(Layout, m(UserForm, vnode.attrs))
+        }
+    },
+})
--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,35 @@ var m = require("mithril")
 var User = {
     list: [],
     loadList: function() {
-        // TODO: make XHR call
+        return m.request({
+            method: "GET",
+            url: "https://rem-rest-api.herokuapp.com/api/users",
+            withCredentials: true,
+        })
+        .then(function(result) {
+            User.list = result.data
+        })
+    },
+
+    current: {},
+    load: function(id) {
+        return m.request({
+            method: "GET",
+            url: "https://rem-rest-api.herokuapp.com/api/users/" + id,
+            withCredentials: true,
+        })
+        .then(function(result) {
+            User.current = result
+        })
+    },
+
+    save: function() {
+        return m.request({
+            method: "PUT",
+            url: "https://rem-rest-api.herokuapp.com/api/users/" + User.current.id,
+            data: User.current,
+            withCredentials: true,
+        })
     }
 }
 
